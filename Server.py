1c18a970b33de384bc8edd614c4d14c76920e6a6
edessa <edessale@gmu.edu>
2015-04-29 12:13:20 -0700

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,12 +1,22 @@
 import socket
+import serial
+import sys
 
-sock = socket.socket()
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-serverAddress = ('192.168.1.69', 9000)
-sock.connect(serverAddress)
+server_address = ('', 10000)
+sock.bind(server_address)
+
+sock.listen(1)
+
+connection, clientAddress = sock.accept() #Not sure if works
+
+ser = serial.Serial("/dev/ttyACM0", 9600)
 
 while True:
-   command = raw_input("Insert here " + sock.recv(0x8000))
-   sock.sendall(command)
-sock.close()
+   connection.sendall("Insert car command here: (a s d w x ")
+   command = connnection.recv(5) #Not sure
+   ser.write(str(command))
 
+connection.close()
+   
